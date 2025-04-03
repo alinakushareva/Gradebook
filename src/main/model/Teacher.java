0f4c8ce8a8b148bc7e124f488faec473c50897a1
Lanyue Zhang <lanyuezhang@arizona.edu>
2025-04-03 06:52:48 -0700

    Update Teacher.java

--- src/main/model/Teacher.java
+++ src/main/model/Teacher.java
@@ -1,5 +1,54 @@
 package main.model;
 
-public class Teacher {
+/*
+ *
+ * Purpose: Represents a teacher in the gradebook system.
+ *          Extends User and adds functionality for managing courses.
+ */
+package main.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Teacher extends User {
+    private List<Course> teachingCourses;
+
+    /*
+     * Constructor for Teacher class.
+     * @param username Unique identifier for the teacher
+     * @param firstName Teacher's first name
+     * @param lastName Teacher's last name
+     * @param passwordHash Hashed password for security
+     */
+    public Teacher(String username, String firstName, String lastName, String passwordHash) {
+        super(username, firstName, lastName, passwordHash, "teacher");
+        this.teachingCourses = new ArrayList<>();
+    }
+
+    /*
+     * Adds a course to the teacher's teaching list.
+     * @param course Course to be added
+     */
+    public void addCourse(Course course) {
+        if (course != null && !teachingCourses.contains(course)) {
+            teachingCourses.add(course);
+            super.addCourse(course); // Ensure it’s also added to the user's courses
+        }
+    }
+
+    /*
+     * Removes a course from the teacher's teaching list.
+     * @param course Course to be removed
+     */
+    public void removeCourse(Course course) {
+        teachingCourses.remove(course);
+    }
+
+    /*
+     * Gets the list of courses the teacher is teaching.
+     * @return List of Course objects
+     */
+    public List<Course> getTeachingCourses() {
+        return new ArrayList<>(teachingCourses); // Return a copy for encapsulation
+    }
 }
