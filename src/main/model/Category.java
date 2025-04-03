d353832061d7993784cfabb4188a75560311b5db
Lanyue Zhang <lanyuezhang@arizona.edu>
2025-04-03 07:37:45 -0700

    Update Category.java

--- src/main/model/Category.java
+++ src/main/model/Category.java
@@ -1,5 +1,22 @@
 package main.model;
 
 public class Category {
+  private String name;
+  private double weight; //(0.0 to 1.0)
+  private List<Assignment> assignments;
+  private int dropLowestCount;//Number of lowest grades to drop
+
+  public Category(){
+    this.weight = weight;
+    this.name = name;
+  }
+  public void addAssignment(Assignment a){}
+  public List<Assignment> getAssignments(){
+    return assignments;
+  }
+  public double getWeight(){
+    return weight;
+  }
+  public double calculateCategoryAverage(Student s){}
 
 }
