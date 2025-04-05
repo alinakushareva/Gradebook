6b4527146234d014819067a047bd2d810c7b0c3e
Lanyue Zhang <lanyuezhang@arizona.edu>
2025-04-05 16:13:12 -0700

    Update Category.java

--- src/main/model/Category.java
+++ src/main/model/Category.java
@@ -9,14 +9,44 @@ public class Category {
   public Category(){
     this.weight = weight;
     this.name = name;
+    this.dropLowestCount = dropLowestCount;
+    this.assignments = new ArrayList<>();
   }
-  public void addAssignment(Assignment a){}
-  public List<Assignment> getAssignments(){
-    return assignments;
+  public void addAssignment(Assignment a) {
+      assignments.add(a);
   }
-  public double getWeight(){
-    return weight;
+
+  public List<Assignment> getAssignments() {
+      return assignments;
   }
-  public double calculateCategoryAverage(Student s){}
+
+  public double getWeight() {
+      return weight;
+  }
+
+  public String getName() {
+      return name;
+  }
+
+  public double calculateCategoryAverage(Student s) {
+      List<Double> percentages = new ArrayList<>();
+      for (Assignment a : assignments) {
+          Grade g = a.getGrade(s);
+          if (g != null) {
+              percentages.add(g.getPercentage());
+          }
+      }
+
+      percentages.sort(Double::compare);
+      for (int i = 0; i < Math.min(dropLowestCount, percentages.size()); i++) {
+          percentages.remove(0);
+      }
+
+      double sum = 0;
+      for (double p : percentages) {
+          sum += p;
+      }
+      return percentages.isEmpty() ? 0 : sum / percentages.size();
+  }
 
 }
